e879fff9c604a2f375ac0f7e40c0016cfeb22855
puff <gu.almeidan2007@gmail.com>
2023-10-14 18:20:05 -0300

    sistema ficha

---
+++ Shadow/sistema ficha/script.js
@@ -0,0 +1,20 @@
+//NOME
+var nome = window.prompt('Nome do personagem')
+document.getElementById('inputNome').innerHTML = nome;
+
+//NIVEL
+var nível = Number(window.prompt('Nível'))
+document.getElementById('inputNível').innerHTML = nível;
+
+//XP
+var xp = Number(window.prompt('Xp'))
+document.getElementById('inputXp').innerHTML = xp;
+
+
+//ATRIBUTOS
+alert('Atributos')
+var força = Number(window.prompt('Força'))
+document.getElementById('inputForça').innerHTML = força;
+
+var Destreza = Number(window.prompt('Destreza'))
+document.getElementById('inputDestreza').innerHTML = Destreza;
