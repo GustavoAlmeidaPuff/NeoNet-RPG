a8625354ec06d02c221b9d54e09de4ea296dea1c
puff <gu.almeidan2007@gmail.com>
2023-10-15 07:01:50 -0300

    sistema ficha 3

---
+++ Shadow/sistema ficha/readme.md
@@ -0,0 +1,3 @@
+# É um sistema para criar fichas NeoNet mais rapidamente
+
+## basta rodar e ele ja vai perguntar o nome do personagem e os atributos
--- Shadow/sistema ficha/script.js
+++ Shadow/sistema ficha/script.js
@@ -1,3 +1,5 @@
+//Math.floor(var)  <--- para arredondar para baixo
+
 //NOME
 var nome = window.prompt('Nome do personagem')//Declara a var "nome" e pede um input pra ela
 document.getElementById('inputNome').innerHTML = nome;//coloca o valor da variável dentro da tabela html
@@ -17,8 +19,8 @@ document.getElementById('inputXp').innerHTML = xp;
 alert('Atributos')
 
 //FORÇA
-var força = Number(window.prompt('Força'))
-document.getElementById('inputForça').innerHTML = força;
+var Força = Number(window.prompt('Força'))
+document.getElementById('inputForça').innerHTML = Força;
 
 //DESTREZA
 var Destreza = Number(window.prompt('Destreza'))
@@ -39,3 +41,66 @@ document.getElementById('inputSabedoria').innerHTML = Sabedoria;
 //MOIRAL
 var Moral = Number(window.prompt('Moral'))
 document.getElementById('inputMoral').innerHTML = Moral;
+
+//CÁLCULO PERÍCIAS
+var perícia = {
+    hacking: Intelecto,
+    engenhariadesistemas: Intelecto,
+    medicina: Sabedoria,
+    persuasão: Moral,
+    investigação: Math.floor((Sabedoria + Intelecto) / 2),
+    acrobacia: Destreza,
+    atletismo: Destreza,
+    percepção: Math.floor((Sabedoria + Destreza) / 2),
+    intuição: Sabedoria,
+    intimidação: Moral,
+    artes: Sabedoria,
+    arremessar: Força,
+    empurrar: Força,
+    vontade: Math.floor((Sabedoria + Constituição) / 2),
+    pesquisa: Intelecto,
+    Pilotagem: Destreza,
+    desarme: Math.floor((Força + Destreza) / 2),
+    furtividade: Destreza
+};
+
+//MANDAR AS PERÍCIAS PARA A TABELA
+document.getElementById('HACKING').innerHTML = perícia.hacking;
+document.getElementById('ENGENHARIA').innerHTML = perícia.engenhariadesistemas;
+document.getElementById('MEDICINA').innerHTML = perícia.medicina;
+document.getElementById('PERSUASÃO').innerHTML = perícia.persuasão;
+document.getElementById('INVESTIGAÇÃO').innerHTML = perícia.investigação;
+document.getElementById('ACROBACIA').innerHTML = perícia.acrobacia;
+document.getElementById('ATLETISMO').innerHTML = perícia.atletismo;
+document.getElementById('PERCEPÇÃO').innerHTML = perícia.percepção;
+document.getElementById('INTUIÇÃO').innerHTML = perícia.intuição;
+document.getElementById('INTIMIDAÇÃO').innerHTML = perícia.intimidação;
+document.getElementById('ARTES').innerHTML = perícia.artes;
+document.getElementById('ARREMESSAR').innerHTML = perícia.arremessar;
+document.getElementById('EMPURRAR').innerHTML = perícia.empurrar;
+document.getElementById('VONTADE').innerHTML = perícia.vontade;
+document.getElementById('PESQUISA').innerHTML = perícia.pesquisa;
+document.getElementById('PILOTAGEM').innerHTML = perícia.Pilotagem;
+document.getElementById('DESARME').innerHTML = perícia.desarme;
+document.getElementById('FURTIVIDADE').innerHTML = perícia.furtividade;
+
+
+/*
+var hacking= Intelecto
+var engenhariadesistemas = Intelecto
+var medicina= Sabedoria
+var persuasão= Moral
+var investigação =  Sabedoria + Intelecto
+var acrobacia= Destreza
+var atletismo= destreza
+var percepção= Sabedoria + Destreza
+var intuição= Sabedoria
+var intimidação= Moral
+var artes= Sabedoria
+var arremessar= Força
+var empurrar= Força
+var vontade= Sabedoria + Constituição
+var pesquisa= Intelecto
+var Pilotagem= Destreza
+var desarme= Força + Destreza
+var furtividade=  Destreza*/
